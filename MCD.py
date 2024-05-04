6688dbcc2be4c20ee60ae8dd82bc0a26384d19bc
Alejandro <72587556@continental.edu.pe>
2024-05-04 18:56:58 -0500

    Tercera version

--- MCD.py
+++ MCD.py
@@ -1,3 +1,5 @@
+import unittest
+
 def mcd(a, b):
     """
     Función para calcular el Máximo Común Divisor (MCD) de dos números.
@@ -7,19 +9,18 @@ def mcd(a, b):
         a, b = b, a % b
     return a
 
-def mcd_cuatro_numeros():
-    """
-    Función para calcular el MCD de cuatro números ingresados por el usuario.
-    """
-    a = int(input("Ingrese el primer número: "))
-    b = int(input("Ingrese el segundo número: "))
-    c = int(input("Ingrese el tercer número: "))
-    d = int(input("Ingrese el cuarto número: "))
-    
+def MinimoComunDivisor(a, b, c, d):
     mcd_ab = mcd(a, b)
     mcd_cd = mcd(c, d)
     return mcd(mcd_ab, mcd_cd)
 
-# Ejemplo de uso
-resultado = mcd_cuatro_numeros()
-print(f"El Máximo Común Divisor de los cuatro números ingresados es: {resultado}")
+class TestMinimoComunDivisor(unittest.TestCase):
+    def test_MinimoComunDivisor(self):
+        """
+        Prueba el caso del minimo comun divisor
+        """
+        resultado_esperado = 12
+        self.assertEqual(MinimoComunDivisor(24,36,48,60), resultado_esperado)
+
+if __name__ == "__main__":
+    unittest.main()
